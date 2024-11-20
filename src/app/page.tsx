ed23f4eb46015bb5267e800281f169633c94ff79
Xiaohua Peng <609370075@qq.com>
2024-11-20 17:21:32 +0800

    feat: complete create app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import { Button } from "@/components/Button"
-
+import { Input } from "@/components/Input"
+import { Textarea } from "@/components/Textarea"
 export default function Home() {
   return (
-    <div><Button variant="ghost">Click me</Button></div>
+    <div className="h-screen flex justify-center items-center">
+      <form action="" className="w-full max-w-xl flex flex-col gap-4">
+        <h1 className="text-center text-2xl font-bold">Create App</h1>
+        <Input name="name" placeholder="App Name"></Input>
+        <Textarea name="description" placeholder="App Description"></Textarea>
+        <Button type="submit">Submit</Button>
+      </form>
+    </div>
   );
 }
