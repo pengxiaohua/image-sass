afd0bb8f617449f05e9014ecae9dcf1bd87e0f3e
Xiaohua Peng <609370075@qq.com>
2025-01-03 00:06:19 +0800

    feat: add zod varify data

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,6 +1,33 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import z from 'zod';
 
-export function GET() {
-  console.log('======>666');
-  return NextResponse.json({ name: 'hello world' });
+const inputSchema = z.object({
+  name: z.string().max(10).min(3),
+  email: z.string().email(),
+});
+
+export function GET(request: NextRequest) {
+  const query = request.nextUrl.searchParams;
+  const name = query.get('name');
+  const email = query.get('email');
+
+  //   try {
+  //     const result = inputSchema.parse({
+  //       name,
+  //       email,
+  //     });
+  //     return NextResponse.json(result);
+  //   } catch (err: any) {
+  //     return NextResponse.json({ error: err.message });
+  //   }
+  const result = inputSchema.safeParse({
+    name,
+    email,
+  });
+  if (result.data) {
+    return NextResponse.json(result.data);
+  }
+  if (result.error) {
+    return NextResponse.json(result.error);
+  }
 }
