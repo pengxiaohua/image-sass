a6cb0a68a9c2131a6e16f89ba900a4b056fae4f9
Xiaohua Peng <609370075@qq.com>
2025-01-15 23:59:19 +0800

    feat: add uppy

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,14 @@
-import { createTRPCContext, serverCaller } from "@/utils/trpc";
+import { Uppy } from '@uppy/core'
+import AWSS3 from '@uppy/aws-s3'
+import { useState } from 'react';
 
 export default async function Home() {
-    const context = await createTRPCContext();
-    const data = await serverCaller(context).hello();
-
+    const [uppy] = useState(() => {
+        const uppy = new Uppy();
+    })
     return (
         <div className="h-screen flex justify-center items-center">
-            Dashboard {data.hello}
+
         </div>
     );
 }
